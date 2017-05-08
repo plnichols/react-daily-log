ee1ee199d96d8a81c6cf1e5dcbc277f4f463beff
Pierre Nichols <pierre.nichols@asos.com>
2017-05-08 09:45:19 +0100

    Add List components files

---
+++ src/components/list/List.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import expect from "expect";
+import { shallow } from "enzyme";
+import List from "./List";
+
+// Unit Under Test
+function uut() {
+	return shallow(<List />);
+}
+
+describe('List', function() {
+	it('should load a list of daily log', function(done) {
+		
+	});
+});
